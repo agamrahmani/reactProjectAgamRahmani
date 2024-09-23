55c9330e01d26ff010c79821fbb93c8fb2b88c10
agam <rahmaniagam2904@gmail.com>
2024-09-23 21:24:10 +0300

    add

--- src/pages/favCards.jsx
+++ src/pages/favCards.jsx
@@ -38,11 +38,8 @@ useEffect(()=>{
 
 useEffect(() => {
     if (user) {
-        // const likes = JSON.parse(localStorage.getItem('likes')) || {};
-        // setLike(likes[user._id]?.[card._id] || false);
         const newLikedCards = cards.filter(card => card.likes.includes(card._id));
         setLikedCards(newLikedCards);
-       
     }
 }, [user, cards]);
 
